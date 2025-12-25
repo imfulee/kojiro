bbc8ff155128ac53161ef32eaf8bb3e6d0c448ab
imfulee <freems5192@gmail.com>
2025-12-25 16:31:40 +0800

    feat: problem 0002

--- 0002/0002.go
+++ 0002/0002.go
@@ -3,6 +3,7 @@ package kojiro
 import (
 	"fmt"
 	"log"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -13,7 +14,7 @@ type ListNode struct {
 }
 
 func (ln *ListNode) String() string {
-	result := []int{}
+	result := []string{}
 	if ln == nil {
 		return ""
 	}
@@ -21,11 +22,15 @@ func (ln *ListNode) String() string {
 		return fmt.Sprintf("%d", ln.Val)
 	}
 
-	for current := ln.Next; current != nil; {
-
+	result = append(result, strconv.Itoa(ln.Val))
+	current := ln.Next
+	for current != nil {
+		result = append(result, strconv.Itoa(current.Val))
+		current = current.Next
 	}
 
-	return result.String()
+	slices.Reverse(result)
+	return strings.Join(result, "")
 }
 
 func NewListNode(number int) *ListNode {
@@ -51,34 +56,74 @@ func NewListNode(number int) *ListNode {
 	return last
 }
 
+func IsSameListNode(src, dst *ListNode) bool {
+	if src == nil && dst == nil {
+		return false
+	}
+
+	srcP := src
+	dstP := dst
+
+	for {
+		if srcP == nil && dstP == nil {
+			break
+		}
+		if srcP == nil || dstP == nil {
+			return false
+		}
+		if srcP.Val != dstP.Val {
+			return false
+		}
+
+		srcP = srcP.Next
+		dstP = dstP.Next
+	}
+
+	return true
+}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	var root *ListNode = nil
 	l1p := l1
 	l2p := l2
 	lastNode := root
+	incrementFromLastStage := 0
 
 	for l1p != nil || l2p != nil {
 		var newNode *ListNode = nil
+		incrSetThisStage := false
+		value := incrementFromLastStage
 
 		if l1p != nil && l2p != nil {
+			value += l1p.Val + l2p.Val
+			digit := value % 10
 			newNode = &ListNode{
-				Val: l1p.Val + l2p.Val,
+				Val: digit,
 			}
 
 			l1p = l1p.Next
 			l2p = l2p.Next
 		} else if l2p == nil {
+			value += l1p.Val
+			digit := value % 10
 			newNode = &ListNode{
-				Val: l1p.Val,
+				Val: digit,
 			}
 
 			l1p = l1p.Next
 		} else if l1p == nil {
+			value += l2p.Val
+			digit := value % 10
 			newNode = &ListNode{
-				Val: l2p.Val,
+				Val: digit,
 			}
 
 			l2p = l2p.Next
+		}
+
+		if value >= 10 {
+			incrementFromLastStage = 1
+			incrSetThisStage = true
 		}
 
 		if lastNode != nil {
@@ -88,6 +133,17 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			root = newNode
 		}
 		lastNode = newNode
+
+		if !incrSetThisStage {
+			incrementFromLastStage = 0
+		}
+	}
+
+	if incrementFromLastStage != 0 {
+		newNode := &ListNode{
+			Val: incrementFromLastStage,
+		}
+		lastNode.Next = newNode
 	}
 
 	return root
--- 0002/0002_test.go
+++ 0002/0002_test.go
@@ -1,11 +1,101 @@
 package kojiro
 
 import (
-	"fmt"
 	"testing"
 )
 
+func TestNewListNode(t *testing.T) {
+	result := NewListNode(1234)
+	expect := &ListNode{
+		Val: 4,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val:  1,
+					Next: nil,
+				},
+			},
+		},
+	}
+
+	if !IsSameListNode(result, expect) {
+		t.Errorf("New list node does not produce the correct ListNode")
+	}
+}
+
+func TestListNodeString(t *testing.T) {
+	result := &ListNode{
+		Val: 4,
+		Next: &ListNode{
+			Val: 9,
+			Next: &ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val:  1,
+					Next: nil,
+				},
+			},
+		},
+	}
+
+	if result.String() != "1594" {
+		t.Errorf("List node string function invalid")
+	}
+}
+
 func TestAddTwoNumbersSameLength(t *testing.T) {
 	result := addTwoNumbers(&ListNode{Val: 1}, &ListNode{Val: 2})
-	fmt.Println(result)
+	if !IsSameListNode(result, &ListNode{Val: 3}) {
+		t.Errorf("Add two numbers of same length incorrect")
+	}
+}
+
+func TestAddTwoNumbersDifferentLength(t *testing.T) {
+	result := addTwoNumbers(&ListNode{Val: 1, Next: &ListNode{Val: 5}}, &ListNode{Val: 2})
+	expect := &ListNode{
+		Val: 3,
+		Next: &ListNode{
+			Val: 5,
+		},
+	}
+	if !IsSameListNode(result, expect) {
+		t.Errorf("Add two numbers of different length incorrect")
+	}
+}
+
+func TestAddTwoNumbersIncrementLength(t *testing.T) {
+	x := &ListNode{Val: 9}
+	y := &ListNode{Val: 7}
+	expect := &ListNode{
+		Val: 6,
+		Next: &ListNode{
+			Val: 1,
+		},
+	}
+	result := addTwoNumbers(x, y)
+	if !IsSameListNode(result, expect) {
+		t.Errorf("Add two numbers to increment length failed")
+	}
+}
+
+func TestAddTwoNumbersCase1(t *testing.T) {
+	x := NewListNode(342)
+	y := NewListNode(465)
+	expect := NewListNode(807)
+	result := addTwoNumbers(x, y)
+	if !IsSameListNode(result, expect) {
+		t.Errorf("342 + 465 should = 807 but equals %s", result)
+	}
+}
+
+func TestAddTwoNumbersCase2(t *testing.T) {
+	x := NewListNode(9999999)
+	y := NewListNode(9999)
+	expect := NewListNode(10009998)
+	result := addTwoNumbers(x, y)
+	if !IsSameListNode(result, expect) {
+		t.Errorf("9999999 + 9999 should = 10009998 but equals %s", result)
+	}
 }
